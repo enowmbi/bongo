1cc2c577456fbed3c5d87a26aa5372b4d258c495
Enow Mbi <benowmbi@yahoo.com>
2023-05-08 07:27:45 +0100

    Change Date.today to Time.zone.today in published scope

--- app/models/bongo/article.rb
+++ app/models/bongo/article.rb
@@ -10,7 +10,7 @@ module Bongo
     field :text, type: String
     field :publish_at, type: Date
 
-    scope :published, -> { where(:publish_at.lte => Date.today) }
+    scope :published, -> { where(:publish_at.lte => Time.zone.today) }
 
     # Mongoid (v7.0.8) doesn't have a cache_version method on its document.
     # This causes https://jira.mongodb.org/browse/MONGOID-4680 when trying to
